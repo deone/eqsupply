e50b235e9031eee4a73e807e74b6fe4161b41a9e
Oladayo Osikoya <alwaysdeone@gmail.com>
2009-07-21 16:54:08 +0100

    Added position to User model, Split company_address to company_street_address, city and country Tested OK.

--- account/models.py
+++ account/models.py
@@ -5,12 +5,23 @@ import datetime
 class User(models.Model):
 	"""
 	# Create users
-	>>> deone = User.objects.create(first_name="Dayo", last_name="Osikoya", email="alwaysdeone@gmail.com", phone="08029299274", username="deone", password="deone", company="Aerix", company_address="9, Olosa Street", reg_id="deonedune369")
-	>>> assert deone.first_name == "Dayo"
-	>>> assert deone.last_name == "Osikoya"
-	>>> assert deone.company_address == "9, Olosa Street"
-	>>> assert deone.reg_id == "deonedune369"
-	>>> assert deone.is_activated == 0
+	>>> import md5
+	>>> hashed = md5.new()
+	>>> hashed.update("deone")
+	>>> new_user = User(first_name="Dayo", last_name="Osikoya", email="alwaysdeone@gmail.com", phone="08029299274", username="deone", password=hashed.hexdigest(), company="Aerix", position="Procurement Officer", company_street_address="9, Olosa Street", city="Lagos", country="", reg_id="deonedune369")
+	>>> assert new_user.first_name == "Dayo"
+	>>> assert new_user.last_name == "Osikoya"
+	>>> assert new_user.email == "alwaysdeone@gmail.com"
+	>>> assert new_user.phone == "08029299274"
+	>>> assert new_user.username == "deone"
+	>>> assert new_user.password == hashed.hexdigest()
+	>>> assert new_user.company == "Aerix"
+	>>> assert new_user.position == "Procurement Officer"
+	>>> assert new_user.company_street_address == "9, Olosa Street"
+	>>> assert new_user.city == "Lagos"
+	>>> assert new_user.country == ""
+	>>> assert new_user.reg_id == "deonedune369"
+	>>> assert new_user.is_activated == 0
 	"""
 	first_name = models.CharField(max_length=30)
 	last_name = models.CharField(max_length=30)
@@ -19,7 +30,10 @@ class User(models.Model):
 	username = models.CharField(max_length=30, unique=True)
 	password = models.CharField(max_length=128)
 	company = models.CharField(max_length=100)
-	company_address = models.CharField(max_length=255)
+	position = models.CharField(max_length=100)
+	company_street_address = models.CharField(max_length=100)
+	city = models.CharField(max_length=100)
+	country = models.CharField(max_length=100, null=True)
 	reg_id = models.CharField(max_length=255)
 	is_activated = models.BooleanField(default=0)
 	date_joined = models.DateTimeField(default=datetime.datetime.now)
@@ -27,3 +41,6 @@ class User(models.Model):
 
 	def __unicode__(self):
 		return self.username
+
+	def activate(self):
+		pass
