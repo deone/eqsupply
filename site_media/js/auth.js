024b089ad3a172b88e2aa69952d13fd93d413f46
Oladayo Osikoya <dayo.osikoya@uknglobal.com>
2009-08-26 11:15:32 +0100

    New generic function ajaxPost() to handle all POST requests.

--- site_media/js/auth.js
+++ site_media/js/auth.js
@@ -2,13 +2,7 @@ $(function()    {
 
 });
 
-// Include ajaxStart() and Stop() functionality
-function login()    {//{{{
-    var username = $("#id_username").val();
-    var password = $("#id_password").val()
-
-    var data = "username=" + username + "&password=" + password;
-    var url = "/account/";
+function ajaxPost(url, data, dLocation)  {
 
     $.ajax({
         url: url,
@@ -23,7 +17,7 @@ function login()    {//{{{
                 if (response.data.type == "error")  {
                     displayErrors(response.data.body);
                 } else  {
-                    document.location = "/products/";
+                    document.location = dLocation;
                 }
             }
         },
@@ -32,4 +26,38 @@ function login()    {//{{{
             alert(response);
         }
     });
+
+}
+
+// Include ajaxStart() and Stop() functionality
+function login()    {//{{{
+    var username = $("#id_username").val();
+    var password = $("#id_password").val()
+
+    var data = "username=" + username + "&password=" + password;
+    var url = "/account/";
+
+    ajaxPost(url, data, "/products/");
+
 }//}}}
+
+function signup()   {
+    var firstname = $("#id_first_name").val();
+    var lastname = $("#id_last_name").val();
+    var email = $("#id_email").val();
+    var phone = $("#id_phone").val();
+    var username = $("#id_username").val();
+    var password1 = $("#id_password1").val();
+    var password2 = $("#id_password2").val();
+    var company = $("#id_company").val();
+    var position = $("#id_position").val();
+    var companyStreetAddress = $("#id_company_street_address").val();
+    var city = $("#id_city").val();
+    var country = $("#id_country").val();
+
+    var data = "first_name=" + firstname + "&last_name=" + lastname + "&email=" + email + "&phone=" + phone + 
+                "&username=" + username + "&password1=" + password1 + "&password2=" + password2 + "&company=" + company + 
+                "&position=" + position + "&company_street_address=" + companyStreetAddress + "&city=" + city + "&country=" + country;
+
+    alert(data);
+}
