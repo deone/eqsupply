3f71ad6575b5215e50f7309bbc3b93ddbd87960c
Oladayo Osikoya <alwaysdeone@gmail.com>
2009-09-02 08:46:42 +0100

    New arg view passed from urls.py into quote_generator.views.view_products_by View function now uses a dict to map view param to appropriate model

--- quote_generator/urls.py
+++ quote_generator/urls.py
@@ -8,6 +8,6 @@ urlpatterns = patterns('',
     url(r'^$', direct_to_template, {"template": "quote_generator/index.html"}, name="products_home"),
     url(r'^manufacturers/', direct_to_template, {"template": "quote_generator/manufacturers.html"}, name="manufacturers"),
     url(r'^categories/', direct_to_template, {"template": "quote_generator/categories.html"}, name="categories"),
-
-    (r'^manufacturer_list/', views.manufacturer_list),
+    (r'^manufacturer_list/', views.view_products_by, {"view": "manufacturer"}),
+    (r'^category_list/', views.view_products_by, {"view": "category"}),
 )
--- quote_generator/views.py
+++ quote_generator/views.py
@@ -3,10 +3,12 @@ from django.core.serializers import serialize
 from django.http import HttpResponse
 
 from eqsupply import helpers as h
-from quote_generator.models import Manufacturer
+from quote_generator.models import Manufacturer, Category
 
-def manufacturer_list(request):
-    manufacturer_list = Manufacturer.objects.all()
-    json = serialize("json", manufacturer_list)
+def view_products_by(request, view):
+    list_model_map = {"manufacturer": Manufacturer, "category": Category}
+
+    list = list_model_map[view].objects.all()
+    json = serialize("json", list)
 
     return HttpResponse(json, mimetype="application/json")
