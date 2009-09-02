904ce109a43bef7a11e5520cb0d6cc745acc50ab
Oladayo Osikoya <alwaysdeone@gmail.com>
2009-09-02 10:26:38 +0100

    Add "http://" to manufacturer website data.

--- quote_generator/sql/data.sql
+++ quote_generator/sql/data.sql
@@ -1,7 +1,7 @@
 use eqsupply;
 
-insert into quote_generator_manufacturer (name, address, city, country, phone, email, website) values ("Elcometer", "2, Surrey Way", "West Ham", "England", "44778938390", "sales@elcometer.com", "www.elcometer.com");
-insert into quote_generator_manufacturer (name, address, city, country, phone, email, website) values ("Zenon", "Plot 2049, Shangzhou Industrial District", "Hong Kong", "China", "44778988388", "sales@zenon.com", "www.zenon.com");
+insert into quote_generator_manufacturer (name, address, city, country, phone, email, website) values ("Elcometer", "2, Surrey Way", "West Ham", "England", "44778938390", "sales@elcometer.com", "http://www.elcometer.com");
+insert into quote_generator_manufacturer (name, address, city, country, phone, email, website) values ("Zenon", "Plot 2049, Shangzhou Industrial District", "Hong Kong", "China", "44778988388", "sales@zenon.com", "http://www.zenon.com");
 
 insert into quote_generator_category (name, short_description) values ("Abrasion Testers", "Lorem Ipsum Dolor noreus");
 insert into quote_generator_category (name, short_description) values ("Adhesion", "Lorem Ipsum Dolor noreus");
