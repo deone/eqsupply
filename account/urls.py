a0fa4dc9403a3ed8166833cd4c150265d3837afe
Oladayo Osikoya <alwaysdeone@gmail.com>
2009-09-30 13:51:05 +0100

    Logout implemented.

--- account/urls.py
+++ account/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns('',
     url(r'^$', views.login, name="acct_login"),
     url(r'^signup/$', views.signup, name="acct_signup"),
     url(r'^activate/', views.activate, name="acct_activate"),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {"template_name": "account/logout.html"}, name="acct_logout"),
     (r'^(?P<user_id>\d+)/pending_quotes/$', views.get_pending_quotes),
 )
