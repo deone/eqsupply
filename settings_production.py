1bd48ac5f9efa7ed4c9f8da4e9496476cbb6e887
Oladayo Osikoya <alwaysdeone@gmail.com>
2009-10-20 14:53:46 +0100

    Set email server username and password to empty in settings_production.py Restart httpd in eqsupply-setup

--- settings_production.py
+++ settings_production.py
@@ -2,6 +2,8 @@ from settings import *
 
 DEBUG = TEMPLATE_DEBUG = False
 
-BASE_URL = "http://eqsupply.aerixnigeria.com/"
+BASE_URL = "http://eqquote.aerixnigeria.com/"
 EMAIL_HOST = "localhost"
 EMAIL_PORT = 25
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
