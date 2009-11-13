b9b4f12e3d07802ad85d56a7dbe8a3f391a46d03
Oladayo Osikoya <alwaysdeone@gmail.com>
2009-11-13 11:28:06 +0100

    Changed email sender address to  dayo@aerixnigeria.com

--- settings.py
+++ settings.py
@@ -108,7 +108,7 @@ BASE_URL = "http://localhost:7700/"
 
 ACTIVATION_EMAIL_SUBJECT = "Your Registration: AERIX EQUIPMENT SUPPLY"
 
-EMAIL_SENDER = "no-reply@aerixnigeria.com"
+EMAIL_SENDER = "dayo@aerixnigeria.com"
 
 ACTIVATION_EMAIL_MESSAGE = "This is your activation link:\n\n%s" 
 
