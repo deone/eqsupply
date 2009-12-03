ce37ee4f2b388f82439c8980e9df3d3d1fd61f69
Oladayo Osikoya <alwaysdeone@gmail.com>
2009-12-03 10:43:03 +0100

    Reduce signup fields to username, password and email

--- account/forms.py
+++ account/forms.py
@@ -1,12 +1,16 @@
 from django import forms
 from django.contrib.auth import authenticate, login
-
-from account.models import UserAccount
+from django.utils.translation import ugettext_lazy as _, ugettext
+from django.contrib.auth.models import User
 
 import hashlib
 import datetime
 import string
 import random
+
+import re
+
+alnum_re = re.compile(r'^\w+$')
 
 class LoginForm(forms.Form):
     username = forms.CharField(label="Username:", max_length=30, widget=forms.TextInput())
@@ -36,37 +40,32 @@ class LoginForm(forms.Form):
         return True
 
 class SignupForm(forms.Form):
-    first_name = forms.CharField(label="First Name:", max_length=30, widget=forms.TextInput())
-    last_name = forms.CharField(label="Last Name:", max_length=30, widget=forms.TextInput())
-    email = forms.EmailField(label="Email:", widget=forms.TextInput())
-    username = forms.CharField(label="Username:", max_length=30, widget=forms.TextInput())
-    password1 = forms.CharField(label="Password:", widget=forms.PasswordInput(render_value=False))
-    password2 = forms.CharField(label="Confirm Password:", widget=forms.PasswordInput(render_value=False))
+    username = forms.CharField(label=_("Username:"), max_length=30, widget=forms.TextInput())
+    password1 = forms.CharField(label=_("Password:"), widget=forms.PasswordInput(render_value=False))
+    password2 = forms.CharField(label=_("Password (again):"), widget=forms.PasswordInput(render_value=False))
+    email = forms.EmailField(label=_("Email:"), widget=forms.TextInput())
+
+    def clean_username(self):
+	if not alnum_re.search(self.cleaned_data["username"]):
+	    raise forms.ValidationError(_("Usernames can only contain letters, numbers and underscores."))
+	try:
+	    user = User.objects.get(username__iexact=self.cleaned_data["username"])
+	except User.DoesNotExist:
+	    return self.cleaned_data["username"]
+	raise forms.ValidationError(_("This username is already taken. Please choose another."))
 
     def clean(self):
-	if self._errors:
-	    return
-	password1 = self.cleaned_data["password1"]
-	password2 = self.cleaned_data["password2"]
-
-	if password1 and password2:
-	    if password1 != password2:
-		raise forms.ValidationError("Your password entries must be the same")
-
+	if "password1" in self.cleaned_data and "password2" in self.cleaned_data:
+	    if self.cleaned_data["password1"] != self.cleaned_data["password2"]:
+		raise forms.ValidationError(_("Your password entries must be the same."))
 	return self.cleaned_data
 
     def save(self):
-	first_name = self.cleaned_data['first_name']
-	last_name = self.cleaned_data['last_name']
-	email = self.cleaned_data['email']
 	username = self.cleaned_data['username']
-	enc_type = "sha1"
-	salt = "".join(random.sample(string.letters+string.digits, 5))
-	password = enc_type + "$" + salt + "$" + hashlib.sha1(salt + self.cleaned_data['password1']).hexdigest()
-	reg_id = hashlib.sha1(email + ":" + password + ":"  + str(datetime.datetime.now())).hexdigest()
-	is_active = 0
+	email = self.cleaned_data['email']
+	password = self.cleaned_data["password1"]
 
-	new_user = UserAccount.objects.create(first_name=first_name, last_name=last_name, email=email, \
-				username=username, password=password, reg_id=reg_id, is_active=is_active)
+	new_user = User.objects.create_user(username, email, password)
+	new_user.is_active = False
 
-	return email, reg_id
+	return email, password
