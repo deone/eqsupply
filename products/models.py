237c97172f7dd0b72e73e4dec78d7117cd2d990a
Oladayo Osikoya <alwaysdeone@gmail.com>
2010-01-27 15:30:29 +0100

    App name change: product to products

---
+++ products/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class CommonInfo(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+
+    class Meta:
+	abstract = True
+
+class Category(CommonInfo):
+    def __unicode__(self):
+	return u"%s" % self.name
+
+    class Meta:
+	verbose_name_plural = "categories"
+
+class Subcategory(CommonInfo):
+    category = models.ForeignKey(Category)
+
+    def __unicode__(self):
+	return u"%s" % self.name
+
+    class Meta:
+	verbose_name_plural = "subcategories"
--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render_to_response, get_object_or_404, get_list_or_404
 from django.template import RequestContext
 
-def index(request, template="product/index.html"):
+def index(request, template="products/index.html"):
     
     return render_to_response(template, {
     }, context_instance=RequestContext(request))
