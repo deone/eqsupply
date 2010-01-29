13f070aac5100445ca7a910ade7ca74e8f3002a5
Oladayo Osikoya <alwaysdeone@gmail.com>
2010-01-29 08:02:55 +0100

    Category and sub-category models. Fetch categories from models to display in template.

--- products/models.py
+++ products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class CommonInfo(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    description = models.TextField()
+    description = models.TextField(blank=True)
 
     class Meta:
 	abstract = True
--- products/views.py
+++ products/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render_to_response, get_object_or_404, get_list_or_404
 from django.template import RequestContext
 
+from products.models import Category
+
+from eqsupply import helpers as h
+
 def index(request, template="products/index.html"):
+    categories = Category.objects.all()
     
     return render_to_response(template, {
+	"categories": categories
     }, context_instance=RequestContext(request))
