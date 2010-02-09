1a7158f57446a9ddae32820843893fee628ef1c0
Oladayo Osikoya <alwaysdeone@gmail.com>
2010-02-09 10:17:44 +0100

    New app 'quote'.

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls.defaults import *
 
 import views
+import quote.views
 
 urlpatterns = patterns("",
     url(r'^$', views.index, name="product_home"),
     (r'^(?P<product_id>\d+)$', views.display_product),
+    (r'^(?P<product_id>\d+)$/quote', quote.views.add_item)
 )
