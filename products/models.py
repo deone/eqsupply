5fb7d7f0a75e23e410c949fdfa931aa77a5098ff
Oladayo Osikoya <alwaysdeone@gmail.com>
2010-02-11 09:04:32 +0100

    ProductVariation model, registered in admin.

--- products/models.py
+++ products/models.py
@@ -43,3 +43,13 @@ class Accessory(models.Model):
 
     class Meta:
 	verbose_name_plural = "accessories"
+
+class ProductVariation(models.Model):
+    """Holds all part numbers and description of products. Many-to-one relationship with Product"""
+    product = models.ForeignKey(Product)
+    part_number = models.CharField(max_length=50, unique=True)
+    description = models.TextField()
+    cost = models.CharField(max_length=10)
+
+    def __unicode__(self):
+	return u"%s - %s" % (self.product, self.part_number)
