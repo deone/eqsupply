c85566eda2f88cbbd0a9dcac9c1bb48c8d7bedfc
Oladayo Osikoya <alwaysdeone@gmail.com>
2010-03-04 15:10:12 +0100

    quote/forms.py needed for add_line_item. Some modifications in js and template.

--- site_media/js/quote.js
+++ site_media/js/quote.js
@@ -1,8 +1,28 @@
+var options = {
+    url: null,
+    type: "POST",
+    data: null,
+    dataType: "json",
+    success: null,
+    error: function(response)   {
+	showMessage("Internal Server Error");
+    }
+};
+
 function showQuoteForm(id) {
     $("#cell" + id).find("#quote_btn").hide();
     $("#cell" + id).find("#quote_form").show();
 }
 
 function quote(pvId)  {
-    alert($("#qty" + pvId).val());
+    var user = $("#user").val();
+    var qty = $("#qty" + pvId).val();
+
+    options["url"] = "/products/" + pvId + "/quote";
+    options["data"] = "user=" + user + "&quantity=" + qty;
+    options["success"] = function(response)	{
+	alert(response);
+    }
+
+    $.ajax(options);
 }
