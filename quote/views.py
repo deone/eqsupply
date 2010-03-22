346ac89abde5c276fc0a6472039a04f8d21fc666
Oladayo Osikoya <alwaysdeone@gmail.com>
2010-03-22 14:09:34 +0100

    Implemented serial no. in line item listing.

--- quote/views.py
+++ quote/views.py
@@ -1,8 +1,9 @@
 from reportlab.pdfgen import canvas
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
 from django.template import RequestContext
 from django.shortcuts import render_to_response, get_object_or_404, get_list_or_404
+from django.contrib.auth.decorators import login_required
 
 from eqsupply.quote.forms import LineItemForm
 from eqsupply.quote.models import *
@@ -11,6 +12,7 @@ from eqsupply import helpers as h
 
 APP_MENU = Division.objects.all()
 
+@login_required
 @h.json_response
 def add_line_item(request, prod_var_id, form_class=LineItemForm, **kwargs):
     form = form_class(request.POST)
@@ -27,10 +29,20 @@ def add_line_item(request, prod_var_id, form_class=LineItemForm, **kwargs):
 
     return h.dict_error(form.errors.items())
 
+@login_required
 def fetch_quote(request, quotation_id, template="quote/quotation.html"):
     quotation = get_object_or_404(Quotation, pk=quotation_id)
+
+    line_item_list = list(quotation.lineitem_set.all())
+
+    i = 0
+
+    while i < int(quotation.lineitem_set.count()):
+	line_item_list[i].id = i + 1
+	i = i + 1
 
     return render_to_response(template, {
 	"menu": APP_MENU,
 	"quotation": quotation,
+	"line_items": line_item_list,
     }, context_instance=RequestContext(request))
