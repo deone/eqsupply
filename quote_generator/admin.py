e7619e160867049d739d554d72406e5dbd6c9a29
Oladayo Osikoya <alwaysdeone@gmail.com>
2010-04-13 14:51:55 +0100

    Removed obsolete app and css files.

--- quote_generator/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from quote_generator.models import *
-
-admin.site.register(Manufacturer)
-admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(Quote)
-admin.site.register(Price)
-admin.site.register(QuoteItem)
--- quote_generator/sql/data.sql
+++
@@ -1,6 +0,0 @@
-use eqsupply;
-
-insert into quote_generator_manufacturer (name, address, city, country, phone, email, website) values ("Elcometer", "2, Surrey Way", "West Ham", "England", "44778938390", "sales@elcometer.com", "http://www.elcometer.com");
-
-insert into quote_generator_category (name, short_description) values ("Abrasion Testers", "A range of instruments designed for wear resistance tests");
-insert into quote_generator_category (name, short_description) values ("Adhesion", "A range of instruments to quantify the strength of the bond between substrate and coating, or between different coating layers or the cohesive strength of some substrates");
